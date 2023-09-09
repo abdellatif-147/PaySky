137cc5e0a4fd1a9152cb0bb58b2cc466ac498c54
abar1 <abdelatif.arafa@mondia.com>
2023-09-10 00:35:18 +0300

    git commit -m "intial impl"

---
+++ README.md
@@ -0,0 +1,27 @@
+I've divide everything into files to be more organized
+
+### `JS file`
+
+i used Vanilla JS and DOM Manipulation to build this page without any framework as per your requirement
+
+i added all logic into this file
+
+### `HTML file`
+
+i added the initial html elements to this page and you can open it just click on it
+
+### `css file`
+
+i added all css into this file
+
+# challenges faced :
+
+Use the same logic to display data whether it is products or categories
+
+Use dom with its limitations to do the same work as any framework
+
+# improvements :
+
+I need to add a loader before the page is initialized before the data arrives
+
+I need to have a dummy API that provides a sort to add with existing logic
---
+++ products.js
@@ -0,0 +1,112 @@
+let increment = 0;
+let total = 0;
+loadCategories();
+loadCarts();
+function loadCategories() {
+  fetch("https://dummyjson.com/products/categories")
+    .then((res) => res.json())
+    .then((res) => {
+      res.forEach((category) => {
+        let categories = document.getElementById("categories");
+        const option = document.createElement("option");
+        option.setAttribute("value", "category");
+        option.innerText = category;
+        categories.appendChild(option);
+      });
+    })
+    .catch((error) => {});
+}
+function selectCategory() {
+  let e = document.getElementById("categories");
+  var text = e.options[e.selectedIndex].text;
+  if (text) {
+    let container = document.querySelector(".grid-container");
+    container.innerHTML = "";
+    loadCarts(0, `https://dummyjson.com/products/category/${text}`);
+  }
+}
+function loadCarts(skipNumber = 0, apiUrl) {
+  skipNumber = skipNumber * increment;
+  increment += 1;
+  let url;
+  if (apiUrl) {
+    url = apiUrl;
+  } else {
+    url = "https://dummyjson.com/products?limit=12";
+    url += `&skip=${skipNumber}`;
+  }
+  let viewMore = document.querySelector(".view-more");
+  if (viewMore) {
+    toggleViewMoreBtn(true);
+    toggleLoader();
+  }
+  fetch(url)
+    .then((res) => res.json())
+    .then((products) => {
+      total = products.total;
+      products.products.forEach((product) => {
+        createCart(product);
+      });
+      toggleViewMoreBtn(false);
+      toggleLoader(true);
+      if (skipNumber == total - 4 || total <= 12) {
+        toggleViewMoreBtn(true);
+      }
+    })
+    .catch((error) => {
+      toggleLoader(true);
+    });
+}
+
+function createCart(product) {
+  const div = document.createElement("div");
+  div.classList.add("grid-item");
+  div.innerHTML = `
+  <img class='image' src='${product.thumbnail}'>
+  <h3 class='title'>${product.title}</h3>
+  <span class='price'>${product.price}</span>
+  <span class='currency'>EGP</span>
+  <button class='add-to-card' data-id='${product.id}' data-title='${product.title}' data-image='${product.thumbnail}' data-price='${product.price}' onclick='addToCart(event)'>Add to Cart</button>
+  `;
+  let container = document.querySelector(".grid-container");
+  container.appendChild(div);
+}
+
+function toggleViewMoreBtn(removeBtn = false) {
+  if (removeBtn) {
+    let viewMore = document.querySelector(".view-more");
+    viewMore?.remove();
+  } else {
+    const button = document.createElement("button");
+    button.innerText = "View More";
+    button.setAttribute("onclick", "loadCarts(12);");
+    button.classList.add("view-more");
+    document.body.appendChild(button);
+  }
+}
+
+function toggleLoader(remove = false) {
+  if (remove) {
+    let loader = document.querySelector(".loader");
+    if (loader) {
+      loader.remove();
+    }
+  } else {
+    const span = document.createElement("span");
+    span.classList.add("loader");
+    document.body.appendChild(span);
+  }
+}
+
+let allSelectedItems = {};
+function addToCart(e) {
+  let selectedItem = {
+    title: e.target.getAttribute("data-title"),
+    price: e.target.getAttribute("data-price"),
+    image: e.target.getAttribute("data-image"),
+    id: e.target.getAttribute("data-id"),
+  };
+  allSelectedItems[selectedItem.id] = selectedItem;
+  localStorage.setItem("allSelectedItems", JSON.stringify(allSelectedItems));
+  e.target.classList.add("dimmed");
+}
